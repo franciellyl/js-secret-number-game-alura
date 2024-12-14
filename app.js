a80b13e83a1db2ffee342a5a935f8eef81f35b5c
franlima <franciellyelima@gmail.com>
2024-12-13 23:29:55 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Welcome to the secret number game!");
+let secretNumber = 7;
+let numberFromUser = prompt("Choose a number between 1 and 15.");
+
+if(secretNumber == numberFromUser){
+    console.log("Awesome! You discovery the secret number! :D");
+}
+
