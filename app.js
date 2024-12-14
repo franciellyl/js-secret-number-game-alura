1bfbb6a797958d2691d465f56a80625ca851e8b6
franlima <franciellyelima@gmail.com>
2024-12-13 23:45:57 -0300

    add: formated success mesage when the user discovery the secret number.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ alert("Welcome to the secret number game!");
 let secretNumber = 7;
 let numberFromUser = prompt("Choose a number between 1 and 15.");
 
-if(secretNumber == numberFromUser){
-    console.log("Awesome! You discovery the secret number! :D");
+if (secretNumber == numberFromUser){
+    alert(`Awesome! You discovery the secret number: ${secretNumber}! :D`);
+} else {
+    alert("Sorry, you're wrong! Try again.")
 }
 
