f08d33503db40760b909289b8e39f460aca88e77
franlima <franciellyelima@gmail.com>
2024-12-14 22:00:05 -0300

    add: added another condition if the first tentative was wrong.

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ if (secretNumber == numberFromUser){
     alert(`Awesome! You discovery the secret number: ${secretNumber}! :D`);
 } else {
     alert("Sorry, you're wrong! Try again.")
+    numberFromUser = prompt("Choose another number between 1 and 15.");
+    if(secretNumber == numberFromUser){
+        alert(`Awesome! You discovery the secret number: ${secretNumber}! :D`);
+    } else {
+        alert("Sorry, you're wrong! Game Over!")
+    }
 }
 
